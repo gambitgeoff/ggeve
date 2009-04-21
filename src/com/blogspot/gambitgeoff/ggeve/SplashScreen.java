033eaa8ad6c41dd704c87c5b3f7e9d14f6e8eeec
Gambit Geoff <=>
2009-04-21 19:06:15 +1000

    Added a SplashScreen to the application.  This is really just to test out features of the API and to work on the process model with GitHub.

---
+++ src/com/blogspot/gambitgeoff/ggeve/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.blogspot.gambitgeoff.ggeve;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+public class SplashScreen extends Activity {
+	
+	private static final int SPLASH_TIMEOUT = 3000;
+	
+	public void onCreate(Bundle inBundle){
+		super.onCreate(inBundle);
+		setContentView(R.layout.splashscreen);
+		new Handler().postDelayed(new Runnable (){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+                Intent mainIntent = new Intent(SplashScreen.this,Menu.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish(); 
+			}}, SPLASH_TIMEOUT);
+	}
+}
