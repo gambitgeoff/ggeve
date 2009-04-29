5e775a60923aa9f06e82938d48fba796c055e1b1
GambitGeoff <gambitgeoff@gmail.com>
2009-04-29 23:25:41 +1000

    Introduced some rudimentary communications to the Eve API.  Now loads in the characters you own when you enter in the API Key and user ID.
    Signed-off-by: GambitGeoff <gambitgeoff@gmail.com>

--- src/com/blogspot/gambitgeoff/ggeve/EveCharacter.java
+++ src/com/blogspot/gambitgeoff/ggeve/EveCharacter.java
@@ -2,9 +2,23 @@ package com.blogspot.gambitgeoff.ggeve;
 
 public class EveCharacter {
 	
-	public EveCharacter()
+	private String myName;
+	private int myCharacterID;
+	
+	public EveCharacter(String inName, int inCharacterID)
 	{
-		
+		myName = inName;
+		myCharacterID = inCharacterID;
+	}
+	
+	public String getName()
+	{
+		return myName;
+	}
+	
+	public int getCharacterID()
+	{
+		return myCharacterID;
 	}
 
 }
--- src/com/blogspot/gambitgeoff/ggeve/GGEveApplicationRunner.java
+++ src/com/blogspot/gambitgeoff/ggeve/GGEveApplicationRunner.java
@@ -8,9 +8,8 @@ public class GGEveApplicationRunner extends Activity {
 
 	private static final int SPLASH_FINISHED = 1453234;
 	public static final String EVE_PREFERENCES = "myEvePreferences";
-	
 	public static final String EVE_PUBLIC_API_KEY = "myEvePublicAPIKey";
-	
+	public static final String EVE_USER_ID = "myEveUserID";
 
 	/** Called when the activity is first created. */
 	public void onCreate(Bundle icicle) {
